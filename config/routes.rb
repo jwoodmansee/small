9759b55e13cbe8044b1efb59698bf2740ab3610c
Eric Durr <emdurr@gmail.com>
2016-08-25 11:54:37 -0600

    routes changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-
-  get 'posts/show'
-
-  get 'posts/new'
-
-  get 'posts/edit'
 
   root 'home#index'
 
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+
+  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
