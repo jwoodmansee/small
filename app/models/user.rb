b27d851e7e894feca94afa00a46a7ca96ddf5a01
Eric Durr <emdurr@gmail.com>
2016-08-25 16:49:57 -0600

    dependent destroy add

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :posts
+  has_many :posts, dependent: :destroy
   has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
   validates_attachment :image, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
 end
