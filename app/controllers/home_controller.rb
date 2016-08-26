09789da5f7c3bd00d3e91f535ed4e262dc9206bc
Eric Durr <emdurr@gmail.com>
2016-08-26 11:57:15 -0600

    styling changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!
   def index
-    @posts = Post.all
+    @posts = Post.all.paginate(:page => params[:page], :per_page => 3)
   end
 end
